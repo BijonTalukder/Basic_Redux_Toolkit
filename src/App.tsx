27a99a25d1a0ff13fdf00cf17c9110cabfe7f304
BijonTalukder <bijontalukder1247@gmail.com>
2023-08-01 11:46:25 +0600

    counter Application with redux done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
+import { useDispatch, useSelector } from "react-redux"
+import { RootState } from "./redux/store"
+import { decrement, increment, incrementByAmount } from "./redux/features/counter/counterSlice"
 
 function App() {
-
+  const {count} = useSelector((state:RootState)=>state.counter)
+ const dispatch = useDispatch()
 
   return (
-    <>
-
-    </>
+    <div>
+      <button onClick={()=>dispatch(increment())}>increment</button>
+      {count}
+      <button onClick={()=>dispatch(decrement())}>decrement</button>
+      <button onClick={()=>dispatch(incrementByAmount(5))}>decrement</button>
+    </div>
   )
 }
 
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,8 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
+import counterReduces from "./features/counter/counterSlice";
 
 
 const store = configureStore({
     reducer:{
-        
+        counter:counterReduces
+
     }
 })
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
+
+export default store
